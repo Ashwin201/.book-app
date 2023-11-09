5af7d86ddd4997fe5bb6ec5001b7651f69dcd194
Ashwin Sharma <sharmaashwin203@gmail.com>
2023-11-09 13:59:59 +0530

    first commit

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -36,7 +36,7 @@ const Post = async ({ id, author, authorEmail, date, thumbnail, category, title,
     const formattedDate = dateObject.toLocaleDateString("en-US", options)
     return (
         <>
-            <div className=' mb-6 mt-6 xl:mb-6 xl:mt-4 cursor-pointer hover:scale-[.99] transition-all duration-500'>
+            <div className=' my-[26px] cursor-pointer hover:scale-[.99] transition-all duration-500'>
                 <Link href={`/post/${id}`} aria-label={title} >
                     <span className='font-medium  text-black dark:text-gray-200 text-sm '>Posted by :<span className='  text-base font-semibold'>&nbsp;{author}</span>
                         &nbsp;on&nbsp;<span className=' text-base  font-semibold'>{formattedDate}</span></span>
@@ -84,7 +84,7 @@ const Post = async ({ id, author, authorEmail, date, thumbnail, category, title,
                                     </div>
                                 )}
 
-                            <div className='flex mb-3 '>
+                            <div className='flex '>
 
                                 <Button href={`/post/${id}`} text="See More" icon={<FiEdit size={18} />} className="bg-slate-200 dark:bg-gray-800 dark:hover:bg-gray-900 mr-3  " />
                                 {
