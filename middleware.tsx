44ef286700a4ef747b37309b43fb272ad338b70a
Ashwin Sharma <sharmaashwin203@gmail.com>
2023-11-09 14:36:56 +0530

    first commit

--- middleware.tsx
+++ middleware.tsx
@@ -5,6 +5,12 @@ export function middleware(request: NextRequest) {
     if (request.nextUrl.pathname === "/post") {
         return NextResponse.rewrite(new URL('/', request.url))
     }
+    if (request.nextUrl.pathname === "/categories") {
+        return NextResponse.rewrite(new URL('/', request.url))
+    }
+    if (request.nextUrl.pathname === "/editpost") {
+        return NextResponse.rewrite(new URL('/', request.url))
+    }
 
 
 }
